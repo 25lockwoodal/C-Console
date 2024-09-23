1c8e7ef9f9c23606e6d8385f198470c53165d20c
25lockwoodal <25lockwoodal@marshallisd.com>
2024-09-23 11:47:55 -0700

    init

--- C#Console/obj/Debug/net8.0/C#Console.AssemblyInfo.cs
+++ C#Console/obj/Debug/net8.0/C#Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e15cc7b152e312bc30f260ae3a6bb4ff31414949")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
